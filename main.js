cd9f3921f403677506546ea1ee57c543dd3243ae
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2021-10-11 12:04:35 +0530

    geting img directory right

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ let types = {
     media: ["mp4", "mkv"],
     archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
     documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
-    app: ['exe', 'dmg', 'pkg', "deb"]
+    images:['jpg','jpeg','png']
 }
 switch (command) {
     case "tree":
