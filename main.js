931e87e297bd8ff7ffd9a30bd3c3e9895116e107
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-09-09 20:13:30 +0530

    made change in utility

--- main.js
+++ main.js
@@ -1,144 +1,50 @@
+#!/usr/bin/env node
+
+// Mkaing the file global
 let inputArr = process.argv.slice(2);
-console.log(inputArr);
 let fs = require("fs");
-let path=require("path");
+let path = require("path");
+let helpObj = require("./commands/help");
+let treeObj = require("./commands/tree");
+let organizeObj = require("./commands/organize");
+
+// console.log(inputArr);
+// node main.js tree "directoryPath"
+// node main.js organize "directoryPath"
+// node main.js help
 let command = inputArr[0];
-
+console.log(command);
 let types = {
-    media: ["mp4", "mkv"],
-    archives: ['zip','7z','rar','tar','gz','ar','iso',"xz"],
-    documents: ['docx','doc','pdf','xlsx','xls','odt','odp','odg','odf','txt','ps','tex'],
-    app: ['exe','dmg','pkg',"deb"]
-}
-
-switch(command)
-{
-    case "tree":
-        treeFn(inputArr[1]);
-        break;
-    case "organise" :
-        organiseFn(inputArr[1]);
-        break;
-    case "help"   :
-        helpFn() ;
-        break;
-    default:
-        console.log("Please input right commands");
-        break;       
-}
-
-function treeFn(dirPath){
-    //let destPath;
-    if(dirPath==undefined)
-    {
-        console.log("Kindly enter the path");
-        return;
-    } 
-    
-    else {
-        let doesExist= fs.existsSync(dirPath);
-
-        if(doesExist){
-          treeHelper(dirPath, "");
-        }
-
-        else {
-            console.log("Kindly enter the correct path");
-            return;
-        }
-    }
-}
-
-function treeHelper(dirPath, indent){
-   let isFile = fs.lstatSync(dirPath).isFile();
-   if(isFile==true){
-       let fileName = path.basename(dirPath);
-       console.log(indent + "|__"+fileName);
-   } else {
-       let dirName=path.basename(dirPath);
-       console.log(indent+"\__"+dirName);
-       let children=fs.readdirSync(dirPath);
-       for(let i=0;i<children.length;i++){
-           let childPath=path.join(dirPath,children[i]);
-           treeHelper(childPath,indent+"\t");
-       }
-   }
-   
-}
-
-function organiseFn(dirPath){
-    let destPath;
-       if(dirPath==undefined)
-       {
-           console.log("Kindly enter the path");
-           return;
-       } 
-       
-       else {
-           let doesExist= fs.existsSync(dirPath);
-
-           if(doesExist){
-              destPath=path.join(dirPath,"organised_Files");
-             if(fs.existsSync(destPath)==false)
-             {fs.mkdirSync(destPath);}
-           }
-
-           else {
-               console.log("Kindly enter the correct path");
-               return;
-           }
-       }
-      organiseHelper(dirPath,destPath);
-}
-
-function organiseHelper(src,dest){
-
-    let childNames = fs.readdirSync(src);
-    for(let i=0;i<childNames.length;i++){
-        let childAddress = path.join(src,childNames[i]);
-        let isFile = fs.lstatSync(childAddress).isFile();
-        if(isFile){
-            // console.log(childNames[i]);
-        let category=getCategory(childNames[i]);
-        console.log(childNames[i],"belongs to -->",category);
-        sendFiles(childAddress,dest,category);
-        }
-    }
-}
-
-function sendFiles(srcFilePath,dest,category){
-    let categoryPath=path.join(dest,category);
-    if(fs.existsSync(categoryPath)==false){
-        fs.mkdirSync(categoryPath);
-    }
-    let fileName = path.basename(srcFilePath);
-    let destFilePath = path.join(categoryPath,fileName);
-    fs.copyFileSync(srcFilePath,destFilePath);
-    fs.unlinkSync(srcFilePath);
-    console.log(fileName,"copied to",category);
-}
-
-function getCategory(name){
- let ext=path.extname(name);
- ext=ext.slice(1);
- for(let type in types){
-     let cTypeArray=types[type];
-     for(let i=0;i<cTypeArray.length;i++)
-     {
-         if(ext==cTypeArray[i])
-         return type;
-     }
- }
- return "others";
-}
-
-
-//help done
-function helpFn(){
-    console.log(`
-    List of All the commands:
-          node main.js tree "directoryPath"
-          node main.js organise "directoryPath"
-          node main.js help
-              `)
+  media: ["mp4", "mkv"],
+  archives: ["zip", "7z", "rar", "tar", "gz", "ar", "iso", "xz"],
+  documents: [
+    "docx",
+    "doc",
+    "pdf",
+    "xlsx",
+    "xls",
+    "odt",
+    "ods",
+    "odp",
+    "odg",
+    "odf",
+    "txt",
+    "ps",
+    "tex",
+  ],
+  images: ["jpg", "jpeg", "png"],
+};
+switch (command) {
+  case "tree":
+    treeObj.treeKey(inputArr[1]);
+    break;
+  case "organize":
+    organizeObj.organizeKey(inputArr[1], types);
+    break;
+  case "help":
+    helpObj.helpKey();
+    break;
+  default:
+    console.log("Please 🙏 Input Right command");
+    break;
 }
--- utility.js
+++ utility.js
@@ -1,8 +1,22 @@
-let utility={}
+let utility = {};
 utility.types = {
-    media: ["mp4", "mkv"],
-    archives: ['zip','7z','rar','tar','gz','ar','iso',"xz"],
-    documents: ['docx','doc','pdf','xlsx','xls','odt','odp','odg','odf','txt','ps','tex'],
-    app: ['exe','dmg','pkg',"deb"]
-}
-module.exports=utility;
+  media: ["mp4", "mkv"],
+  archives: ["zip", "7z", "rar", "tar", "gz", "ar", "iso", "xz"],
+  documents: [
+    "docx",
+    "doc",
+    "pdf",
+    "xlsx",
+    "xls",
+    "odt",
+    "odp",
+    "odg",
+    "odf",
+    "txt",
+    "ps",
+    "tex",
+  ],
+  images: ["jpg", "jpeg", "png"],
+  gifs: ["gif"],
+};
+module.exports = utility;
